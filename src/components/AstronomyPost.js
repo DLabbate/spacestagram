4e1e3238154261aea554e671f2426980fc8ee2bd
Domenic Labbate <domeniclabbate@gmail.com>
2021-09-13 22:44:15 -0400

    Lazy loading images with react-intersection-observer

--- src/components/AstronomyPost.js
+++ src/components/AstronomyPost.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { FiHeart } from "react-icons/fi";
 import "./AstronomyPost.css";
+import { useInView } from "react-intersection-observer";
+
 const AstronomyPost = ({
   title,
   description,
@@ -11,23 +13,33 @@ const AstronomyPost = ({
   addLike,
   removeLike,
 }) => {
-  return (
-    <div className="w-auto m-4 shadow-lg rounded-2xl inline-block relative bg-white">
-      {mediaType === "video" ? (
-        <iframe
-          className="object-cover rounded-t-2xl w-full"
-          src={url}
-          alt="Astronomy"
-          title={title}
-          allowFullScreen
-        ></iframe>
-      ) : (
-        <img
-          className="object-cover rounded-t-2xl w-full"
-          src={url}
-          alt="Astronomy"
-        />
-      )}
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: "200px 0px",
+  });
+
+  const Media = () => {
+    return mediaType === "video" ? (
+      <iframe
+        className="object-cover rounded-t-2xl w-full"
+        src={url}
+        alt="Astronomy"
+        title={title}
+        allowFullScreen
+        loading="lazy"
+      ></iframe>
+    ) : (
+      <img
+        className="object-cover rounded-t-2xl w-full"
+        src={url}
+        alt="Astronomy"
+        loading="lazy"
+      />
+    );
+  };
+
+  const Content = () => {
+    return (
       <div className="m-6">
         <h3 className="text-lg font-bold">{title}</h3>
         <h4 className="text-sm">{date}</h4>
@@ -47,6 +59,16 @@ const AstronomyPost = ({
           />
         </button>
       </div>
+    );
+  };
+  return (
+    <div
+      className="w-auto m-4 shadow-lg rounded-2xl inline-block relative bg-white"
+      ref={ref}
+    >
+      {inView ? <Media /> : null}
+
+      <Content />
     </div>
   );
 };
